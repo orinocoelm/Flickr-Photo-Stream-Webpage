e700f33c4ca745700ef3fbb2eeca62c07638575e
Colin Moore <col@orinocoelm.com>
2017-06-14 18:52:51 +0100

    Setup file structure and Bootstrap.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+			App: "./app/assets/scripts/App.js",
+			Vendor: "./app/assets/scripts/Vendor.js"
+		 },
+  output: {
+    path: path.resolve(__dirname, 'app/temp/scripts'),
+    filename: '[name].js'
+  },
+  module: {
+	  loaders: [
+		{
+		  loader: 'babel-loader',
+		  query: {
+			  presets: ['es2015']
+		  },
+		  test: /\.js$/,
+		  exclude: /node_modules/
+		}
+	  ]
+  },
+  resolve: {
+	  modules: [
+		path.resolve(__dirname, 'app'),
+		path.resolve(__dirname, 'app/assets/scripts/modules'),
+		path.resolve(__dirname, 'node_modules')
+	  ]
+  }
+};
